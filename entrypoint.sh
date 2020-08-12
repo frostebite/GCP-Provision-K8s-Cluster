d6cdacff9099e37c62339eb635fd274087a11fef
Frostebite <jas.f.ukcmti@gmail.com>
2020-08-12 01:24:32 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,15 @@
 #!/bin/sh -l
 
+# load configuration file
+
+# decode all base64 input files from the authentication input
+
+# get cluster status command
+
+# create cluster command
+
+# get kube credentials command
+
 echo "Hello $1"
 time=$(date)
 echo ::set-output name=time::$time
